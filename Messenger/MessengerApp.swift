41d9de8a859741166a4e4c8cc1abb4fc6db5fc93
Ujjwal Arora <ukarora9@gmail.com>
2024-09-21 17:03:17 +0530

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Ujjwal Arora on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
